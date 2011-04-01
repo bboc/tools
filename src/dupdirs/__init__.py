c94bd0552f4f36c4bc6ab94d79b940d735c16f57
bboc <bboc@gmx.net>
2011-04-02 00:55:59 +0200

    added package dupdirs

---
+++ src/dupdirs/__init__.py
@@ -0,0 +1,11 @@
+"""
+Find duplicate (sub)directories.
+
+Look at:
+
+- `dircompare <http://code.google.com/p/dircompare/>`_
+- `filecmp <http://docs.python.org/library/filecmp.html>`_
+- `dircmp <http://docs.python.org/library/filecmp.html#filecmp.dircmp>`_
+
+"""
+
