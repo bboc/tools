d4100d5e3dcd22068d45f753a2f6271e0935fa00
bboc <bboc@gmx.net>
2011-04-12 18:23:10 +0200

    refactored to a class

--- src/dupdirs/__main__.py
+++ src/dupdirs/__main__.py
@@ -114,109 +114,121 @@ class DuplicateSet(object):
             return res
 
 
-@cli.app.CommandLineApp
-def find_duplicates(app):
-    """
-    Find duplicate directories in a list of given dirs.
-    """
-    start_time = datetime.now()
-    factory = Factory()
+class FindDuplicatesDirs(cli.app.CommandLineApp):
 
-    def verbose(*args):
-        if app.params.verbose:
-            print(*args)
+    def main(self):
 
-    # build up all directory trees
-    for root in app.params.root:
-        verbose('looking for duplicates in', root)
-        DirTree(root, factory, app.params.mtime, app.params.symlink_warning)
+        """
+        Find duplicate directories in a list of given dirs.
+        """
+        start_time = datetime.now()
+        factory = Factory()
 
-    # build all duplicates (may still contain nested duplicates)
-    dirs_by_digest = defaultdict(DuplicateSet)
-    for item in factory.values():
-        dirs_by_digest[item.digest].add(item)
+        def verbose(*args):
+            if self.params.verbose:
+                print(*args)
 
-    # get all actual duplicates
-    duplicates = [item for item in dirs_by_digest.values() if len(item.items) > 1]
-    # order duplicate sets by size
-    duplicates.sort(key=lambda ds: ds.size, reverse=False)
+        # build up all directory trees
+        for root in self.params.root:
+            verbose('looking for duplicates in', root)
+            DirTree(root, factory, self.params.mtime, self.params.symlink_warning)
 
-    if not app.params.no_nested_duplicates:
-        verbose('\n\nall duplicates found:', len(duplicates))
-        duplicates = _eliminate_nested_duplicates(duplicates)
+        # build all duplicates (may still contain nested duplicates)
+        dirs_by_digest = defaultdict(DuplicateSet)
+        for item in factory.values():
+            dirs_by_digest[item.digest].add(item)
 
-    if app.params.limit_results:
-        # slice the biggest few
-        start = len(duplicates) - app.params.limit_results
-        duplicates = duplicates[start:]
+        # get all actual duplicates
+        duplicates = [item for item in dirs_by_digest.values() if len(item.items) > 1]
+        # order duplicate sets by size
+        duplicates.sort(key=lambda ds: ds.size, reverse=False)
 
-    # show results
-    for d in duplicates:
-        print(d)
-        if app.params.dircmp:
-            d.dircmp()
-        if app.params.filecmp:
-            if d.filecmp():
-                print('-->identical duplicates verified')
+        if not self.params.no_nested_duplicates:
+            verbose('\n\nall duplicates found:', len(duplicates))
+            duplicates = self._eliminate_nested_duplicates(duplicates)
+
+        if self.params.limit_results:
+            # slice the biggest few
+            start = len(duplicates) - self.params.limit_results
+            duplicates = duplicates[start:]
+
+        # show results
+        for d in duplicates:
+            print(d)
+            if self.params.dircmp:
+                d.dircmp()
+            if self.params.filecmp:
+                if d.filecmp():
+                    print('-->identical duplicates verified')
+                else:
+                    print('WARNING: differences detected!!')
+
+
+        print('\n\nduplicates found:', len(duplicates))
+
+        delta = datetime.now() - start_time
+        print('time elapsed', delta)
+        return 0
+
+
+
+    def _eliminate_nested_duplicates(self, duplicates):
+        """
+        This will most likely be not very effective for most data sets, so it is
+        optional.
+        """
+        # eliminate all nested duplicates
+        real_duplicates = []
+        for idx, smaller_item in enumerate(duplicates):
+            for larger_item in duplicates[idx+1:]:
+                if larger_item.contains(smaller_item):
+                    break
             else:
-                print('WARNING: differences detected!!')
+                real_duplicates.append(smaller_item)
+        return real_duplicates
 
+    def setup(self):
+        super(FindDuplicatesDirs, self).setup()
+        self.add_param("-v", "--verbose",
+                                  help="more verbose output",
+                                  default=False, action="store_true")
 
-    print('\n\nduplicates found:', len(duplicates))
+        self.add_param("-d", "--dircmp",
+                                  help="use dircmp to verify results (after applying the limit)",
+                                  default=False, action="store_true")
 
-    delta = datetime.now() - start_time
-    print('time elapsed', delta)
-    return 0
+        self.add_param("-f", "--filecmp",
+                                  help="use filecmp on duplicate sets to verify results (after applying the limit)",
+                                  default=False, action="store_true")
 
+        self.add_param("-s", "--symlink-warning",
+                                  help="warn when encountering symlinks (default behavoiur is exit on first symlink found)",
+                                  default=False, action="store_true")
 
+        self.add_param("-m", "--mtime",
+                                  help="include last modifieds time in detection of duplicates",
+                                  default=False, action="store_true")
 
-def _eliminate_nested_duplicates(duplicates):
-    """
-    This will most likely be not very effective for most data sets, so it is
-    optional.
-    """
-    # eliminate all nested duplicates
-    real_duplicates = []
-    for idx, smaller_item in enumerate(duplicates):
-        for larger_item in duplicates[idx+1:]:
-            if larger_item.contains(smaller_item):
-                break
-        else:
-            real_duplicates.append(smaller_item)
-    return real_duplicates
+        self.add_param("-l", "--limit-results",
+                                  help="limit number of displayed results to n (default is all)",
+                                  type=int, default=0, action="store")
 
+        self.add_param("-o", "--output",
+                                  help="output file for interactive deletion",
+                                  action="store")
 
-find_duplicates.add_param("-v", "--verbose",
-                          help="more verbose output",
-                          default=False, action="store_true")
+        self.add_param("-i", "--input",
+                                  help="input file for interactive deletion, contents of file are processed and deleted.",
+                                  action="store")
 
-find_duplicates.add_param("-d", "--dircmp",
-                          help="use dircmp to verify results (after applying the limit)",
-                          default=False, action="store_true")
+        self.add_param("-n", "--no-nested-duplicates",
+                                  help="don't detect nested duplicates",
+                                  default=False, action="store_true")
 
-find_duplicates.add_param("-f", "--filecmp",
-                          help="use filecmp on duplicate sets to verify results (after applying the limit)",
-                          default=False, action="store_true")
-
-find_duplicates.add_param("-s", "--symlink-warning",
-                          help="warn when encountering symlinks (default behavoiur is exit on first symlink found)",
-                          default=False, action="store_true")
-
-find_duplicates.add_param("-m", "--mtime",
-                          help="include last modifieds time in detection of duplicates",
-                          default=False, action="store_true")
-
-find_duplicates.add_param("-l", "--limit-results",
-                          help="limit number of displayed results to n (default is all)",
-                          type=int, default=0, action="store")
-
-find_duplicates.add_param("-n", "--no-nested-duplicates",
-                          help="don't detect nested duplicates",
-                          default=False, action="store_true")
-
-find_duplicates.add_param("root", nargs='+', help="path(s) to search for duplicates")
+        self.add_param("root", nargs='+', help="path(s) to search for duplicates")
 
 
 if __name__ == '__main__':
-    find_duplicates.run()
+    fd =FindDuplicatesDirs()
+    fd.run()
 
