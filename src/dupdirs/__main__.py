93921a5c8fc1dc3a725d807e3bba731792652413
bboc <bboc@gmx.net>
2011-04-13 01:16:00 +0200

    option for reversed processing, reordered args alphabetically

--- src/dupdirs/__main__.py
+++ src/dupdirs/__main__.py
@@ -118,7 +118,7 @@ class FindDuplicatesDirs(cli.app.CommandLineApp):
 
         # get all actual duplicates
         duplicates = [item for item in dirs_by_digest.values() if len(item.items) > 1] # order duplicate sets by size
-        duplicates.sort(key=lambda ds:ds.size, reverse=False)
+        duplicates.sort(key=lambda ds:ds.size, reverse=self.params.reverse)
         return duplicates
 
     def _eliminate_nested_duplicates(self, duplicates):
@@ -149,8 +149,9 @@ class FindDuplicatesDirs(cli.app.CommandLineApp):
     def setup(self):
         """Define commandline parameters and messages."""
         super(FindDuplicatesDirs, self).setup()
-        self.add_param("-v", "--verbose",
-                                  help="more verbose output",
+
+        self.add_param("-c", "--commit",
+                                  help="actually do the deletions (only used with --input)",
                                   default=False, action="store_true")
 
         self.add_param("-d", "--dircmp",
@@ -161,31 +162,32 @@ class FindDuplicatesDirs(cli.app.CommandLineApp):
                                   help="use filecmp on duplicate sets to verify results (after applying the limit)",
                                   default=False, action="store_true")
 
-        self.add_param("-s", "--symlink-warning",
-                                  help="warn when encountering symlinks (default behavoiur is exit on first symlink found)",
-                                  default=False, action="store_true")
-
-        self.add_param("-m", "--mtime",
-                                  help="include last modifieds time in detection of duplicates",
-                                  default=False, action="store_true")
-        self.add_param("-c", "--commit",
-                                  help="actually do the deletions (only used with --input)",
-                                  default=False, action="store_true")
+        self.add_param("-i", "--input",
+                                  help="input file for interactive deletion, contents of file are processed and deleted.",
+                                  action="store")
 
         self.add_param("-l", "--limit-results",
                                   help="limit number of displayed results to n (default is all)",
                                   type=int, default=0, action="store")
 
-        self.add_param("-o", "--output",
-                                  help="output file for interactive deletion",
-                                  action="store")
-
-        self.add_param("-i", "--input",
-                                  help="input file for interactive deletion, contents of file are processed and deleted.",
-                                  action="store")
+        self.add_param("-m", "--mtime",
+                                  help="include last modifieds time in detection of duplicates",
+                                  default=False, action="store_true")
 
         self.add_param("-n", "--no-nested-duplicates",
                                   help="don't detect nested duplicates",
+                                  default=False, action="store_true")
+
+        self.add_param("-r", "--reverse",
+                                  help="reverse output of duplicates (largest duplicate first)",
+                                  default=False, action="store_true")
+
+        self.add_param("-s", "--symlink-warning",
+                                  help="warn when encountering symlinks (default behavoiur is exit on first symlink found)",
+                                  default=False, action="store_true")
+
+        self.add_param("-v", "--verbose",
+                                  help="more verbose output",
                                   default=False, action="store_true")
 
         self.add_param("root", nargs='*', help="path(s) to search for duplicates")
